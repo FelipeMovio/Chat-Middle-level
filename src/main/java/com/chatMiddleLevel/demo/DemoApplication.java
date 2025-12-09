a28a52c76636d5ff5651436450f9378648f3fb34
FelipeMovio <felipelsmovio@gmail.com>
2025-12-09 19:41:33 -0300

    create project

---
+++ src/main/java/com/chatMiddleLevel/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.chatMiddleLevel.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
