e475e139d2cd10577ebc685a169ca7711206bf58
FelipeMovio <felipelsmovio@gmail.com>
2025-12-09 20:14:38 -0300

    update

--- src/main/java/com/chatMiddleLevel/demo/controller/ChatController.java
+++ src/main/java/com/chatMiddleLevel/demo/controller/ChatController.java
@@ -1,4 +1,18 @@
 package com.chatMiddleLevel.demo.controller;
 
+import com.chatMiddleLevel.demo.DTOs.ChatInputDTO;
+import com.chatMiddleLevel.demo.DTOs.ChatOutputDTO;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
 public class ChatController {
+
+    @MessageMapping("/new-message")
+    @SendTo("/topics/livechat")
+    public ChatOutputDTO message(ChatInputDTO input){
+        return new ChatOutputDTO(HtmlUtils.htmlEscape(input.user() + " : " + input.message()));
+    }
 }
