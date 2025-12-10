71bcd2d3d4e476c6499de4580f1367404b4b8608
FelipeMovio <felipelsmovio@gmail.com>
2025-12-10 19:22:58 -0300

    update

--- src/main/java/com/chatMiddleLevel/demo/repository/UserRepository.java
+++ src/main/java/com/chatMiddleLevel/demo/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.chatMiddleLevel.demo.repository;
 
-public interface UserRepository {
+import com.chatMiddleLevel.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
